ad04bc5399f6588866d68bb38ea900f9b290746c
Toan Nguyen <28768308+toann1980@users.noreply.github.com>
2024-10-09 21:08:59 -0700

    01 build initial (#2)
    * Add pyproject.toml file
    
    * Add getfiles package and GetFiles module
    
    * Refactor update readme.md
    
    * Ignore Visual Studio Code settings

---
+++ getfiles/GetFiles.py
@@ -0,0 +1,214 @@
+from collections.abc import Generator
+from datetime import datetime as dt
+import os
+from pathlib import Path
+from typing import Literal
+
+
+def get_files(
+    path: Path | str,
+    extensions: str | list[str] = None,
+    subfolders: bool = True,
+    time_type: Literal["created", "modified", None] = "modified",
+    as_date_time: bool = False,
+    str_format: str = "%Y-%m-%d %H:%M:%S",
+    size: bool = False
+) -> list[dict] | list[str]:
+    """
+    Returns a list of files in the given path with optional additional
+    information.
+
+    Args:
+        path (str | Path): The path to search for files.
+        extensions (str | list[str], optional): The file extensions to include.
+            If a string is provided, it can be a single extension or multiple
+            extensions separated by commas. Defaults to None, which includes
+            all files.
+        subfolders (bool, optional): Whether to include files in subfolders.
+            Defaults to True.
+        time_type (Literal["created", "modified", None], optional): The type of
+            time to return. If None, will not return the time.
+        as_date_time (bool, optional): Whether to return the time as a datetime
+            object. If False, the time is returned as a string formatted
+            according to `str_format`. Defaults to False.
+        str_format (str, optional): The format to use for the date time string
+            if `as_date_time` is False. Defaults to "%Y-%m-%d %H:%M:%S".
+        size (bool, optional): Whether to return the size of the files.
+            Defaults to False.
+
+    Returns:
+        list[dict | str]: A list of files in the given path. If `as_date_time`
+            or `size` is True, each file is represented as a dictionary
+            with the file path and the requested additional information.
+            Otherwise, each file is represented as a string with the file path.
+    """
+    if isinstance(path, str):
+        path = Path(path)
+
+    if not os.access(path, os.R_OK):
+        raise PermissionError(f"No read permission for directory {path}.")
+
+    if isinstance(extensions, str):
+        extensions = (extensions,) if "," not in extensions else \
+            tuple([ext.strip() for ext in extensions.split(",")])
+
+    return list(
+        get_files_iterator(
+            path, extensions, subfolders, time_type, as_date_time, str_format,
+            size
+        )
+    )
+
+
+def get_files_iterator(
+    path: Path | str,
+    extensions: str | list[str],
+    subfolders: bool,
+    time_type: Literal["created", "modified", None],
+    as_date_time: bool,
+    str_format: str,
+    size: bool
+) -> Generator[Path | dict]:
+    """
+    Returns a generator that yields files in the given path.
+
+    Args:
+        path (str | Path): The path to search for files.
+        extensions (str | list[str], optional): The file extensions to include.
+        subfolders (bool, optional): Whether to include subfolders.
+        time_type (Literal["created", "modified", None], optional): The type of
+            time to return. If None, will not return the time.
+        as_date_time (bool, optional): Whether to return the time as a datetime
+            object.
+        str_format (str, optional): The format to use for the date time string.
+        size (bool, optional): Whether to return the size of the files.
+
+    Yields:
+        Generator[str | dict[str]]: A generator that yields files in the given
+            Path or dict[str] given, the choices. If only the path is chosen, a
+            Path will be returned.
+    """
+    try:
+        with os.scandir(path) as iterator:
+            for file in iterator:
+                if file.is_dir(follow_symlinks=False) and subfolders:
+                    yield from get_files_iterator(
+                        Path(file), extensions, subfolders, time_type,
+                        as_date_time, str_format, size
+                    )
+                elif extensions is None or \
+                        file.name.lower().endswith(tuple(extensions)):
+                    yield process_file(
+                        Path(file), time_type, as_date_time, str_format,
+                        size
+
+                    )
+    except FileNotFoundError:
+        print(f"Directory {path} does not exist.")
+    except PermissionError:
+        print(f"Permission denied for directory {path}.")
+
+
+def get_folder_and_file_count(path: str) -> tuple[int, int]:
+    """
+    Returns the number of folders and files in the given path.
+
+    Args:
+        path (str): The path to count the folders and files in.
+
+    Returns:
+        tuple[int, int]: A tuple where the first element is the number of
+            folders and the second element is the number of files.
+    """
+    num_folders = 0
+    num_files = 0
+
+    def scan_directory(path: str):
+        nonlocal num_folders, num_files
+        with os.scandir(path) as iterator:
+            for entry in iterator:
+                if entry.is_dir(follow_symlinks=False):
+                    num_folders += 1
+                    scan_directory(entry.path)
+                elif entry.is_file(follow_symlinks=False):
+                    num_files += 1
+
+    scan_directory(path)
+
+    return num_folders, num_files
+
+
+def get_folder_size(path: str) -> int:
+    """
+    Returns the size of a folder in bytes.
+
+    Args:
+        path (str): The path to the folder.
+
+    Returns:
+        int: The size of the folder in bytes.
+    """
+    total_size = 0
+
+    def scan_directory(path: str):
+        nonlocal total_size
+        with os.scandir(path) as iterator:
+            for entry in iterator:
+                if entry.is_dir(follow_symlinks=False):
+                    scan_directory(entry.path)
+                elif entry.is_file(follow_symlinks=False):
+                    total_size += entry.stat(follow_symlinks=False).st_size
+
+    scan_directory(path)
+
+    return total_size
+
+
+def process_file(
+    path: Path,
+    time_type: Literal["created", "modified", None],
+    as_date_time: bool,
+    str_format: str,
+    size: bool
+) -> Path | dict:
+    """Processes a file and returns its information.
+
+    Args:
+        path (Path): The file to process.
+        time_type (Literal["created", "modified", None], optional): The type of
+            time to return. If None, will not return the time.
+        as_date_time (bool): If True, returns the time as a datetime object.
+            If False, returns the time as a string formatted according to
+            `str_format`.
+        str_format (str, optional): The format to use for the date time string
+            if `as_date_time` is False.
+        size (bool): If True, includes the size of the file in the returned
+            information.
+
+    Returns:
+        Path | dict: A dictionary containing the information of the file. The
+            dictionary includes the path of the file, the specified time (either
+            'modified' or 'created'), and optionally the size of the file. If no
+            additional information is requested (`as_date_time` and `size`
+            are both False), the function returns the path.
+    """
+    info = {"path": path}
+
+    if time_type is not None:
+        if time_type == "modified":
+            file_time = dt.fromtimestamp(
+                path.stat(follow_symlinks=False).st_mtime
+            )
+        else:
+            file_time = dt.fromtimestamp(
+                path.stat(follow_symlinks=False).st_ctime
+            )
+        if as_date_time:
+            info["datetime"] = file_time
+        else:
+            info["datetime"] = file_time.strftime(str_format)
+
+    if size:
+        info["size"] = path.stat(follow_symlinks=False).st_size
+
+    return info if len(info.keys()) > 1 else path
